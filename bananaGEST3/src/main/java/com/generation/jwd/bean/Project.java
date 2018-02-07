ed4079fd274d1c7db58537aeaa331222ab8adccb
NOxtradamuZ <borjahz92@gmail.com>
2018-02-07 17:18:04 +0100

    Add Project

--- bananaGEST3/src/main/java/com/generation/jwd/bean/Project.java
+++ bananaGEST3/src/main/java/com/generation/jwd/bean/Project.java
@@ -1,12 +1,34 @@
 package com.generation.jwd.bean;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="project")
 public class Project {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
 	private int id;
+	
+	@Column(name="name")
 	private String name;
+	
+	@Column(name="description")
 	private String description;
+	
+	@Column(name="date_start")
 	private String date_start;
+	
+	@Column(name="date_end")
 	private String date_end;
+	
+	@Column(name="id_user")
 	private int id_user;
 	
 	public Project() {};
