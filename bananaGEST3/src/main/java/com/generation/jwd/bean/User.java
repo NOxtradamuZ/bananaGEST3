24fece1b5b27d0f99809e2ee39b9d9d849188cbc
NOxtradamuZ <borjahz92@gmail.com>
2018-02-07 17:16:57 +0100

    Fix User

--- bananaGEST3/src/main/java/com/generation/jwd/bean/User.java
+++ bananaGEST3/src/main/java/com/generation/jwd/bean/User.java
@@ -1,7 +1,5 @@
 package com.generation.jwd.bean;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,15 +7,18 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="user")
-public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class User {
+		
 	@Id
 	@Column(name = "id")	
 	private int id;
+	
 	@Column(name = "name")
 	private String name;
+	
 	@Column(name = "email")
 	private String email;
+	
 	@Column(name = "password")	
 	private String password;
 	
