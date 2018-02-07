3850e16ea982ff3cc9826c0d332d69414b9b1e9b
NOxtradamuZ <borjahz92@gmail.com>
2018-02-07 17:33:03 +0100

    Add LoginController

---
+++ bananaGEST3/src/main/java/com/generation/jwd/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.generation.jwd.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+	@RequestMapping(method=RequestMethod.GET)
+}
